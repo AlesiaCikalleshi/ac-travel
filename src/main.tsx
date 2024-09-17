d93dc39174c2037e40b1d41901de646ba7a237e5
AlesiaCikalleshi <118731784+AlesiaCikalleshi@users.noreply.github.com>
2024-09-17 22:49:05 +0200

    initial project setup

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
