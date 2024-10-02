2335736e4ce4dcf1aee10a31cccf6e86c1ca08d1
AlesiaCikalleshi <118731784+AlesiaCikalleshi@users.noreply.github.com>
2024-10-02 13:50:40 +0200

    feat: App router and protected router

--- src/app/config/routes/components/AppRouter.tsx
+++ src/app/config/routes/components/AppRouter.tsx
@@ -1,23 +1,39 @@
 import { Route, Routes } from "react-router-dom";
 
 import AuthLayout from "@features/ui/layout/AuthLayout";
+import AddTripPage from "@pages/account/add-trip";
 import DashboardPage from "@pages/account/dashboard";
+import TripsPage from "@pages/account/trips";
 import HomePage from "@pages/home";
 import LoginPage from "@pages/login";
 import NotFoundPage from "@pages/not-found";
 import SignUpPage from "@pages/sign-up";
 
-import { AppRoutes } from "./AppRoutes";
+import { AppRoutes } from "../AppRoutes";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function AppRouter() {
   return (
     <Routes>
+      {/* Public Pages */}
       <Route path={AppRoutes.home} element={<HomePage />} />
       <Route element={<AuthLayout />}>
         <Route path={AppRoutes.signUp} element={<SignUpPage />} />
         <Route path={AppRoutes.login} element={<LoginPage />} />
       </Route>
-      <Route path={AppRoutes.dashboard} element={<DashboardPage />} />
+      {/* Protected Account Pages */}
+      <Route
+        element={
+          <ProtectedRoute>
+            <AuthLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route path={AppRoutes.dashboard} element={<DashboardPage />} />
+        <Route path={AppRoutes.trips} element={<TripsPage />} />
+        <Route path={AppRoutes.addTrip} element={<AddTripPage />} />
+      </Route>
+      {/* Not Found Page */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
