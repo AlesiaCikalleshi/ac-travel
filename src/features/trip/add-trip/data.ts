4fa8538e016f4725c985268b2b04d165080d9eb8
AlesiaCikalleshi <118731784+AlesiaCikalleshi@users.noreply.github.com>
2024-10-06 21:35:05 +0200

    feat: adding wizard layout

---
+++ src/features/trip/add-trip/data.ts
@@ -0,0 +1,53 @@
+import TravelInfo from "./components/Steps/TravelInfo";
+import { WizardSteps } from "./types";
+
+export const WIZARD_STEPS: WizardSteps[] = [
+  {
+    title: "Select campaign settings",
+    description: `For each ad campaign that you create.`,
+    Component: TravelInfo,
+  },
+  {
+    title: "Create an ad group",
+    description:
+      "An ad group contains one or more ads which target a shared set of keywords.",
+    Component: TravelInfo,
+  },
+  {
+    title: "Create an ad",
+    description: `Try out different ad text to see what brings in the most customers,
+                and learn how to enhance your ads using features like ad extensions.
+                If you run into any problems with your ads, find out how to tell if
+                they're running and how to resolve approval issues.`,
+    Component: TravelInfo,
+  },
+  {
+    title: "Selecs",
+    description: `For each ad campaign that you create, you can control how much
+                you're willing to spend oon clicks and conversions, which networks
+                and geographical locations you want your ads to show on, and more.`,
+    Component: TravelInfo,
+  },
+  {
+    title: "Creat",
+    description:
+      "An ad group contains one or more ads whoich target a shared set of keywords.",
+    Component: TravelInfo,
+  },
+  {
+    title: "Cr",
+    description: `Try out different ad text to see what brings in the most customers,
+                and learn how to enhance your ads uosing features like ad extensions.
+                If you run into any problems with your ads, find out how to tell if
+                they're running and how to resolve approval issues.`,
+    Component: TravelInfo,
+  },
+  {
+    title: "Creatt",
+    description: `Try out different ad texto to see what brings in the most customers,
+                and learn how to enhance your ads using features like ad extensions.
+                If you run into any problems with your ads, find out how to tell if
+                they're running and how to resolve approval issues.`,
+    Component: TravelInfo,
+  },
+];
